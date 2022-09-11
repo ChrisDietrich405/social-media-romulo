d289c73e0c164cff8e2facf8dfa9c2bf64df2523
Chris <chrisdietrich3666@gmail.com>
2022-09-11 11:55:13 -0500

    added first links for header

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// import { logo } from "../assets/logo.png"
+
+const Header = () => {
+  return (
+    <header>
+        {/* <img src={}>logo</img> */}
+        <nav>
+            <Link to="#">
+
+            </Link>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Home.tsx
@@ -0,0 +1,75 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+// import React from 'react'
+// import PostItem from './PostItem'
+
+
+
+// const Home = () => {
+//     const posts = [
+//         {
+//             id: 1,
+//             title: "post 1",
+//             author: "Romulo",
+//             comments: [
+//                 {
+//                     id: 1,
+//                     author: "Chris",
+//                     message: "great post",
+//                     like: 1
+//                 },
+//                 {
+//                     id: 2,
+//                     author: "Milena",
+//                     message: "great post",
+//                     like: 1
+//                 },
+//             ]
+//         },
+//         {
+//             id: 2,
+//             title: "post 2",
+//             author: "Paulleth",
+//             comments: [
+//                 {
+//                     id: 1,
+//                     author: "Christina",
+//                     message: "great post",
+//                     like: 1
+//                 },
+//                 {
+//                     id: 2,
+//                     author: "Junior",
+//                     message: "great post",
+//                     like: 1
+//                 },
+//             ]
+//         }
+//     ]
+//   return (
+//     <main>
+//         <h1>hello</h1>
+//         {/* <ul>
+//             {posts.map((post) => {
+//                 return (
+//                     <li>
+//                         <PostItem post={post}/>
+//                     </li>
+//                 )
+//             })}
+            
+//             </ul> */}
+//     </main>
+//   )
+// }
+
+// export default Home
